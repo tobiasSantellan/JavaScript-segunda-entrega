30ae140a9708596e6ec1a3e59505998f7d065c07
tobiasSantellan <tobias.ariel.santellan@gmail.com>
2023-07-14 19:51:49 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const pizzas = [
+    {
+      id: 1,
+      nombre: "pizza de Muzzarella",
+      precio: 500,
+      ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
+    },
+  
+    {
+      id: 2,
+      nombre: "pizza de Cebolla",
+      precio: 1500,
+      ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
+    },
+  
+    {
+      id: 3,
+      nombre: "pizza Napolitana",
+      precio: 1350,
+      ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas"],
+    },
+  
+    {
+      id: 4,
+      nombre: "pizza 4 Quesos",
+      precio: 1380,
+      ingredientes: [
+        "Muzzarella",
+        "Tomate",
+        "Queso Azul",
+        "Parmesano",
+        "Roquefort",
+      ],
+    },
+  
+    {
+      id: 5,
+      nombre: "pizza Especial",
+      precio: 1000,
+      ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas", "Cebolla"],
+    },
+  
+    {
+      id: 6,
+      nombre: "pizza con Anana",
+      precio: 600,
+      ingredientes: ["Muzzarella", "Tomate", "Anana"],
+    },
+  ];
